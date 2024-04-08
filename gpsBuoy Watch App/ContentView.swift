0a2a1a759772e11083534ef557e5c14e65194eb3
jshigu <info@yskeo.com>
2024-04-08 22:58:34 +0900

    Initial Commit

---
+++ gpsBuoy Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gpsBuoy Watch App
+//
+//  Created by 世吉 on 2024/04/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gpsBuoy Watch App/gpsBuoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  gpsBuoyApp.swift
+//  gpsBuoy Watch App
+//
+//  Created by 世吉 on 2024/04/08.
+//
+
+import SwiftUI
+
+@main
+struct gpsBuoy_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

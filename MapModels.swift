894867e2d1f3b5a00713690ea20b52276d734c6e
jshigu <info@yskeo.com>
2024-04-11 04:13:07 +0900

    GPSブイ登録

---
+++ MapModels.swift
@@ -0,0 +1,66 @@
+//
+//  MapModels.swift
+//  gpsBuoy Watch App
+//
+//  Created by 世吉 on 2024/04/08.
+//
+
+import WatchKit
+import MapKit
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    // CLLocationManagerをインスタンス化
+    let manager = CLLocationManager()
+    
+    // 更新のたびに変化するので@Publishedを付与して観測
+    @Published  var region =  MKCoordinateRegion()
+    
+    
+    override init() {
+        super.init()                                        // スーパクラスのイニシャライザを実行
+        manager.delegate = self                             // 自身をデリゲートプロパティに設定
+        manager.requestWhenInUseAuthorization()             // 位置情報を利用許可を要求
+        manager.desiredAccuracy = kCLLocationAccuracyBest   // 最高精度の位置情報を要求
+        manager.distanceFilter = 3.0                        // 更新距離(m)
+        manager.startUpdatingLocation()                     //現在位置アップデートの生成を開始
+    }
+    
+    // 領域の更新をするデリゲートメソッド
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // 配列の最後に最新のロケーションが格納される
+        // map関数を使って全要素にアクセス map{ $0←要素に参照 }
+        locations.last.map {
+            let center = CLLocationCoordinate2D(
+                latitude: $0.coordinate.latitude,
+                longitude: $0.coordinate.longitude)
+            
+            // 地図を表示するための領域を再構築
+            region = MKCoordinateRegion(
+                center: center,
+                latitudinalMeters: 100.0,
+                longitudinalMeters: 100.0
+            )
+        }
+    }
+    
+    func reloadRegion (bouyNo: Int){
+        // オプショナルバインディング
+        if let location = manager.location {
+            
+            let center = CLLocationCoordinate2D(
+                latitude: location.coordinate.latitude,
+                longitude: location.coordinate.longitude
+            )
+            
+            pointList[bouyNo] = Point(latitude: location.coordinate.latitude , longitude: location.coordinate.longitude )
+            
+            region = MKCoordinateRegion(
+                center: center,
+                latitudinalMeters: 100.0,
+                longitudinalMeters: 100.0
+            )
+        }
+    }
+    
+}
--- gpsBuoy Watch App/ContentView.swift
+++ gpsBuoy Watch App/ContentView.swift
@@ -6,19 +6,83 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+struct Point: Identifiable {
+    let id = UUID()         //ユニークID
+    let latitude: Double    // 緯度
+    let longitude: Double   // 経度
+    // 座標
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
     }
 }
 
-#Preview {
-    ContentView()
+var pointList = [
+    Point(latitude: 35.709152712026265, longitude: 139.80771829999996),
+    Point(latitude: 35.711554715026265, longitude: 139.81371829999996),
+    Point(latitude: 35.712527719026265, longitude: 139.81071829999996)
+]
+
+struct ContentView: View {
+    @ObservedObject  var manager = LocationManager()
+    
+    //ユーザートラッキングモードを追従モード変数 .follow　ユーザーを追跡  .none　ユーザーの追跡を停止
+    @State  var trackingMode = MapUserTrackingMode.follow
+    
+    var body: some View {
+        ZStack{
+            Map(coordinateRegion: $manager.region,  //状態変数をバインディング指定
+                showsUserLocation: true, // マップ上にユーザーの場所を表示するオプションをBool値で指定
+                userTrackingMode: $trackingMode,
+                annotationItems: pointList,
+                annotationContent: {
+                point in MapPin(    //「マーカー(MapMarker)」「ピン(MapPin)」
+                    coordinate: point.coordinate,
+                    tint: .orange   //色
+                )
+            }
+            )
+            .edgesIgnoringSafeArea(.bottom)
+            .edgesIgnoringSafeArea(.top)
+            VStack{
+                Spacer()
+                HStack{
+                    Button(action: {
+                        manager.reloadRegion(bouyNo: 0)
+                    }) {
+                        Image(systemName: "pencil.circle.fill")
+                            .foregroundColor(.white)
+                            .font(.system(size: 16))
+                    }
+                    .frame(width: 30, height: 25)
+                    .cornerRadius(30.0)
+                    
+                    Button(action: {
+                        manager.reloadRegion(bouyNo: 1)
+                    }) {
+                        Image(systemName: "mappin.circle.fill")
+                            .foregroundColor(.white)
+                            .font(.system(size: 16))
+                    }
+                    .frame(width: 30, height: 25)
+                    .cornerRadius(30.0)
+                    
+                    Button(action: {
+                        manager.reloadRegion(bouyNo: 2)
+                    }) {
+                        Image(systemName: "mappin.square.fill")
+                            .foregroundColor(.white)
+                            .font(.system(size: 16))
+                    }
+                    .frame(width: 30, height: 25)
+                    .cornerRadius(30.0)
+                    
+                }
+                .background(Color(red: 0.4, green: 0.5, blue: 0.2))   //背景色
+                .cornerRadius(30.0)
+            }
+        }
+    }
 }
+

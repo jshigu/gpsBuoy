7cd919f7061dc027301ae974d95938430b50f6b8
jshigu <info@yskeo.com>
2024-04-25 09:18:10 +0900

    ボタンを絵文字に変更

--- MapModels.swift
+++ MapModels.swift
@@ -8,9 +8,15 @@
 import WatchKit
 import MapKit
 
+
+ func deg2rad(_ deg:CGFloat ) -> Double {
+    return CGFloat.pi / 180.0 * deg
+}
+
 class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     // CLLocationManagerをインスタンス化
     let manager = CLLocationManager()
+    var locationDistanceFlag = 500
     
     // 更新のたびに変化するので@Publishedを付与して観測
     @Published  var region =  MKCoordinateRegion()
@@ -24,27 +30,93 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
         manager.distanceFilter = 3.0                        // 更新距離(m)
         manager.startUpdatingLocation()                     //現在位置アップデートの生成を開始
     }
-    
-    // 領域の更新をするデリゲートメソッド
+     
+        // 領域の更新をするデリゲートメソッド
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
         // 配列の最後に最新のロケーションが格納される
         // map関数を使って全要素にアクセス map{ $0←要素に参照 }
         locations.last.map {
             let center = CLLocationCoordinate2D(
                 latitude: $0.coordinate.latitude,
                 longitude: $0.coordinate.longitude)
-            
-            // 地図を表示するための領域を再構築
-//            region = MKCoordinateRegion(
-//                center: center,
-//                latitudinalMeters: 500.0,
-//                longitudinalMeters: 500.0
-//            )
+            if locationDistanceFlag == 500 {
+                manager.startUpdatingLocation()
+                //地図を表示するための領域を再構築
+                region = MKCoordinateRegion(
+                    center: center,
+                    latitudinalMeters: 600.0,
+                    longitudinalMeters: 600.0
+                )
+            }
+            else if locationDistanceFlag == 200 {
+                manager.startUpdatingLocation()
+                region = MKCoordinateRegion(
+                    center: center,
+                    latitudinalMeters: 200,
+                    longitudinalMeters: 200
+                )
+            }
+            else if locationDistanceFlag == 0 {
+                manager.stopUpdatingLocation()
+            }
         }
     }
     
-    func reloadRegion (bouyNo: Int){
+    
+    func locationmanagerStop()->Bool{
+        locationDistanceFlag = 0
+        let status = manager.authorizationStatus
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+                                                        //3:authorizedAlways 常に利用許可
+                                                        //4:authorizedAlways 使用中のみ利用許可
+            manager.stopUpdatingLocation()
+        case .notDetermined:                            //0:notDetermined 未選択
+            manager.requestWhenInUseAuthorization()
+        case .denied:                                   //2: 利用拒否
+            //アラートを表示して、アプリの位置情報サービスをオンにするように促す
+            return false
+        case .restricted:                               //1:ペアレンタルコントロールなどの影響で制限中
+            break
+        default:
+            break
+        }
+        return true
+    }
+    
+    func locationmanagerStart() -> Bool{
+        locationDistanceFlag = 500
+        let status = manager.authorizationStatus
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            manager.startUpdatingLocation()
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        case .denied:
+            return false
+        case .restricted:
+            break
+        default:
+            break
+        }
+        return true
+    }
+
+    func locationDistance() -> Int {
+        if let location = manager.location {
+            let a = CGFloat.pi / 180.0 * pointList[0].latitude
+            let b = CGFloat.pi / 180.0 * pointList[0].longitude
+            let c = CGFloat.pi / 180.0 * location.coordinate.latitude
+            let d = CGFloat.pi / 180.0 * location.coordinate.longitude
+            let e = Int( cal_distance(x1: a, y1: b, x2: c, y2: d) * 1000.0 ) % 10000
+            return( e )
+
+        }else{
+            return( 0 )
+        }
+    }
+    
+    func reloadRegion (bouyNo: Int)-> Bool{
         // オプショナルバインディング
         if let location = manager.location {
             
@@ -52,7 +124,7 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
                 latitude: location.coordinate.latitude,
                 longitude: location.coordinate.longitude
             )
-
+            
             if bouyNo == 0 || bouyNo == 1 || bouyNo == 2 {
                 //GPSブイ投下を記録
                 pointList[bouyNo] = Point(
@@ -60,19 +132,67 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
                     latitude: location.coordinate.latitude ,
                     longitude: location.coordinate.longitude
                 )
-                region = MKCoordinateRegion(
-                    center: center,
-                    latitudinalMeters: 50.0,
-                    longitudinalMeters: 50.0
-                )
-            } else {
-                //ロケーションボタン
-                region = MKCoordinateRegion(
-                    center: center,
-                    latitudinalMeters: 200.0,
-                    longitudinalMeters: 200.0
-                )
+            }else{
+                if bouyNo == 3 {
+                    locationDistanceFlag = 200
+                    region = MKCoordinateRegion(
+                        center: center,
+                        latitudinalMeters: 200.0,
+                        longitudinalMeters: 200.0
+                    )
+                }else if bouyNo == 4 {
+                    locationDistanceFlag = 500
+                    region = MKCoordinateRegion(
+                        center: center,
+                        latitudinalMeters: 600.0,
+                        longitudinalMeters: 600.0
+                    )
+                }
             }
         }
+        //位置情報サービスがデバイス上で有効になっているか?
+        if CLLocationManager.locationServicesEnabled() {
+            let status = manager.authorizationStatus
+            switch status {
+            case .authorizedAlways, .authorizedWhenInUse:
+                //ユーザーは、アプリがいつでも位置情報サービスを開始することを許可している
+                //ユーザーは、アプリの使用中に位置情報サービスを開始することを許可した
+                //この時は
+                //ユーザーの現在位置を報告するアップデートの生成を開始
+                manager.startUpdatingLocation()
+            case .notDetermined:
+                //アプリの使用中に位置情報サービスを使用する許可をユーザーに要求
+                manager.requestWhenInUseAuthorization()
+            case .denied:
+                //ユーザーがアプリの位置情報サービスの使用を拒否しているので、アラートで知らせる
+                return false
+            case .restricted:
+                //このアプリは位置情報サービスを使用する権限がない
+                break
+            default:
+                break
+            }
+        }else {
+            //ユーザーがアプリの位置情報サービスの使用を拒否しているので、アラートで知らせる
+            return false
+        }
+        return true
     }
 }
+
+
+
+private func cal_distance(x1: Double, y1: Double, x2: Double, y2: Double)->(Double) {
+    let dx = x2 - x1
+    let dy = y2 - y1
+    let mu = (y1 + y2) / 2.0 // μ
+    let RX = 6378.137; // 回転楕円体の長半径（赤道半径）[km]
+    let RY = 6356.752; // 回転楕円体の短半径（極半径) [km]
+    let E = sqrt(1 - pow(RY / RX, 2.0)) // 離心率
+    let W = sqrt(1 - pow(E * sin(mu), 2.0))
+    let M = RX * (1 - pow(E, 2.0)) / pow(W, 3.0) // 子午線曲率半径
+    let N = RX / W // 卯酉線曲率半径
+    return sqrt(pow(M * dy, 2.0) + pow(N * dx * cos(mu), 2.0)) // 距離[km]
+}
+
+
--- gpsBuoy Watch App/ContentView.swift
+++ gpsBuoy Watch App/ContentView.swift
@@ -27,22 +27,17 @@ var pointList = [
 
 struct ContentView: View {
     @ObservedObject  var manager = LocationManager()
-    
+    @State private var isShowAlert = false
     //ユーザートラッキングモードを追従モード変数 .follow　ユーザーを追跡  .none　ユーザーの追跡を停止
     @State  var trackingMode = MapUserTrackingMode.follow
-    
+    @State  var showsUserLocationFlag: Bool = true
+    @State  var isOn:Bool = false
     var body: some View {
         ZStack{
             Map(coordinateRegion: $manager.region,  //状態変数をバインディング指定
-                showsUserLocation: true, // マップ上にユーザーの場所を表示するオプションをBool値で指定
+                showsUserLocation: showsUserLocationFlag, // マップ上にユーザーの場所を表示するオプションをBool値で指定
                 userTrackingMode: $trackingMode,
                 annotationItems: pointList,
-//                annotationContent: {
-//                    point in MapPin(    //「マーカー(MapMarker)」「ピン(MapPin)」
-//                        coordinate: point.coordinate,
-//                        tint: .orange   //色
-//                    )
-//                }
                 annotationContent: { (pointList) in
                     MapAnnotation(coordinate: pointList.coordinate) {
                         VStack {
@@ -57,64 +52,159 @@ struct ContentView: View {
                 }
             )
             .edgesIgnoringSafeArea(.all)
-            .edgesIgnoringSafeArea(.bottom)
-            .edgesIgnoringSafeArea(.top)
+            
+            
+
             VStack{
+
+                HStack{
+                    let dis = manager.locationDistance()
+                    Text("\(dis)")
+                        .foregroundColor(.white)
+                        .font(.system(size: 14))
+
+                    Button(action: {
+                        showsUserLocationFlag = true
+                        if false == manager.locationmanagerStart(){
+                            isShowAlert.toggle()
+                        }
+                    }) {
+                        Image(systemName: "location")
+                            .foregroundColor(.white)
+                            .font(.system(size: 18))
+
+//                        Text("On")
+//                            .foregroundColor(.white)
+//                            .font(.system(size: 11))
+                    }
+                    .frame(width: 34, height: 23)
+                    .cornerRadius(30.0)
+                    
+                    Button(action: {
+                        showsUserLocationFlag = false
+                        if false == manager.locationmanagerStop(){
+                            isShowAlert.toggle()
+                        }
+                    }) {
+                        Image(systemName: "location.slash")
+                            .foregroundColor(.white)
+                            .font(.system(size: 18))
+
+//                        Text("Off")
+//                            .foregroundColor(.white)
+//                            .font(.system(size: 11))
+                    }
+                    .frame(width: 34, height: 23)
+                    .cornerRadius(40.0)
+                }
+                .background(Color(red: 0.4, green: 0.5, blue: 0.2))   //背景色
+                .cornerRadius(30.0)
+                .offset(x: -20, y: -38)
+                
+                
                 Spacer()
                 HStack{
                      Button(action: {
-                        manager.reloadRegion(bouyNo: 0)
+                         if false == manager.reloadRegion(bouyNo: 0){
+                             isShowAlert.toggle()
+                         }
                     }) {
-                        Image(systemName: "mappin.circle.fill")
+                        Image(systemName: "p1.button.horizontal")
                             .foregroundColor(.white)
-                            .font(.system(size: 12))
-                        Text("1")
-                            .foregroundColor(.white)
-                            .font(.system(size: 12))
+                            .font(.system(size: 23))
+//                        Image(systemName: "mappin.circle.fill")
+//                            .foregroundColor(.white)
+//                            .font(.system(size: 12))
+//                        Text("1")
+//                            .foregroundColor(.white)
+//                            .font(.system(size: 12))
                     }
-                    .frame(width: 41, height: 25)
+                    .frame(width: 36, height: 23)
                     .cornerRadius(30.0)
                     
                     Button(action: {
-                        manager.reloadRegion(bouyNo: 1)
+                        if false == manager.reloadRegion(bouyNo: 1){
+                            isShowAlert.toggle()
+                        }
                     }) {
-                        Image(systemName: "mappin.circle.fill")
+                        Image(systemName: "p2.button.horizontal")
                             .foregroundColor(.white)
-                            .font(.system(size: 12))
-                        Text("2")
-                            .foregroundColor(.white)
-                            .font(.system(size: 12))
+                            .font(.system(size: 23))
+
+//                        Image(systemName: "mappin.circle.fill")
+//                            .foregroundColor(.white)
+//                            .font(.system(size: 12))
+//                        Text("2")
+//                            .foregroundColor(.white)
+//                            .font(.system(size: 12))
                     }
-                    .frame(width: 41, height: 25)
+                    .frame(width: 36, height: 23)
                     .cornerRadius(30.0)
                     
                     Button(action: {
-                        manager.reloadRegion(bouyNo: 2)
+                        // No.2 GPSブイを登録する際、GPS情報入手可能でない時、isShowAlertでアラート発生
+                        //          manager.reloadRegion(bouyNo: 2)
+                        if false == manager.reloadRegion(bouyNo: 2){
+                            isShowAlert.toggle()
+                        }
                     }) {
-                        Image(systemName: "mappin.circle.fill")
+                        Image(systemName: "p3.button.horizontal")
                             .foregroundColor(.white)
-                            .font(.system(size: 12))
-                        Text("3")
-                            .foregroundColor(.white)
-                            .font(.system(size: 12))
+                            .font(.system(size: 23))
+
+//                        Image(systemName: "mappin.circle.fill")
+//                            .foregroundColor(.white)
+//                            .font(.system(size: 12))
+//                        Text("3")
+//                            .foregroundColor(.white)
+//                            .font(.system(size: 12))
                     }
-                    .frame(width: 41, height: 25)
+                    .frame(width: 36, height: 23)
                     .cornerRadius(30.0)
-                    
-                    
+
                     Button(action: {
-                        manager.reloadRegion(bouyNo: 3)
+                        if isOn == false {
+                            if false == manager.reloadRegion(bouyNo: 3){
+                                isShowAlert.toggle()
+                            }
+
+//                            manager.reloadRegion(bouyNo: 3)
+                            isOn = true
+                        }else{
+                            if false == manager.reloadRegion(bouyNo: 4){
+                                isShowAlert.toggle()
+                            }
+
+//                            manager.reloadRegion(bouyNo: 4)
+                            isOn = false
+                        }
                     }) {
-                        Image(systemName: "location.fill")
-                            .foregroundColor(.white)
-                            .font(.system(size: 16))
+                        if isOn == true {
+                            Image(systemName: "minus.magnifyingglass")
+                                .foregroundColor(.white)
+                                .font(.system(size: 22))
+                        }else{
+                            Image(systemName: "plus.magnifyingglass")
+                                .foregroundColor(.white)
+                                .font(.system(size: 22))
+                        }
                     }
-                    .frame(width: 32, height: 25)
+                    .frame(width: 26, height: 23)
                     .cornerRadius(30.0)
                     
                 }
                 .background(Color(red: 0.4, green: 0.5, blue: 0.2))   //背景色
                 .cornerRadius(30.0)
+                .offset(x: 0, y: 17)
+                
+                //OkAlertを記載
+                .alert("Error", isPresented: $isShowAlert) {
+                    // ダイアログ内で行うアクション処理...
+                } message: {
+                    // アラートのメッセージ...
+                    Text("アプリの位置情報サービスを\niPhoneから\nオンにして下さい\n設定>プライバシとセキュリティ>位置情報サービス＞gpsBuoy")
+                }
+
             }
         }
     }
